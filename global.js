1e13367fbb60d6746342d2a6db98b159b6f1ba30
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-21 20:46:02 +0200

    how to get path of the current directory and current file

--- global.js
+++ global.js
@@ -2,11 +2,16 @@
 
 //console.log(global);
 
-/*global.*/setTimeout(() => {
-    console.log('in the timeout');
-    clearInterval(interval); // stops our interval
-}, 3000);
+// /*global.*/setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(interval); // stops our interval
+// }, 3000);
 
-const interval = setInterval(() => {
-    console.log('in the interval');
-}, 1000);
+// const interval = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+
+// gets the absolute path of the current folder without file in which it works
+console.log(__dirname); 
+// gets the absolute path of the folder with the file name added on as well
+console.log(__filename); 
