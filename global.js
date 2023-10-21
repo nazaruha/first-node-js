9eca58b1ca6613d12764253e29ab84f21bca0119
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-21 20:42:18 +0200

    set setInterval func

--- global.js
+++ global.js
@@ -4,4 +4,9 @@
 
 /*global.*/setTimeout(() => {
     console.log('in the timeout');
+    clearInterval(interval); // stops our interval
 }, 3000);
+
+const interval = setInterval(() => {
+    console.log('in the interval');
+}, 1000);
