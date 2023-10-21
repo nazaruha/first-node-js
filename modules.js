0b8e47135fbda81150276a42dacb20c8722b98ec
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-21 21:17:35 +0200

    check built-in object from node.js -> os

--- modules.js
+++ modules.js
@@ -1,9 +1,14 @@
 // const xyz = require('./people.js'); // require method automatically finds and runs the file
 // console.log(xyz.people, xyz.ages);
 
-// another export way
+// another way export way
 const { people, ages }  = require("./people");
 
 console.log(people, ages);
+
+const os = require('os'); // built-in file from node.js
+// good object if need to find out info about your Operating System
+
+console.log(os.platform(), os.homedir());
 
 
