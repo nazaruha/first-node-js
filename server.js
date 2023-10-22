dfd99cc52850ce5e218f882831f634b88c9a8739
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-22 13:03:07 +0200

    send simple text response to the browser

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader("Content-Type", "text/plain" /*sending some text to the browser*/);
+    res.write("hello, ninjas"); // what content send to the browser
+    res.end(); // end the response
 }); // method to create a server
 
 server.listen(3000 /*port*/, 'localhost'/*ip*/, () => {
