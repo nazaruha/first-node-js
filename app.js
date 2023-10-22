645257f6989daec9fae18f5031e6298a73386057
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-22 15:44:54 +0200

    how to export html files into web using sendFile method

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express();
 app.listen(3000); // automatically infers to the localhost
 
 app.get("/", (req, res) => {
-    // this send method automatically recognises what kind of data you want to send
-    res.send("<p>home page</p>"); 
+    // this `send` method automatically recognises what kind of data you want to send
+    //res.send("<p>home page</p>"); 
+
+    // `sendFile` method - how to easy export html files.
+    // BUT you need to pass the root directory, 'cause express doesn't where from to start
+    res.sendFile("./views/index.html", {root: __dirname});
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile("./views/about.html", {root: __dirname});
 })
