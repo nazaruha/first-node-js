c0adf0726a77955099641a9380948b2f2f1677a5
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-22 12:06:11 +0200

    how to pass a data into the stream

--- streams.js
+++ streams.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream("./docs/blog3.txt", {encoding: 'utf8'}); // encoding we need to get data in a propriate way
+const writeStream = fs.createWriteStream("./docs/blog4.txt", {encoding: 'utf8'});
 
 // 'on' method is an event listener
 readStream.on('data', (chunk) => {
     console.log("------ NEW CHUNK ------");
     console.log(chunk);
+    writeStream.write("\nNEW  CHUNK\n"); // how to pass data into stream
+    writeStream.write(chunk); // how to pass data into stream
 })
