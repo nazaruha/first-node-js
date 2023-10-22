12e079b36c4ebec43f217a1314cde86bab8bdac1
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-22 22:17:20 +0200

    add more js code into ejs

--- app.js
+++ app.js
@@ -11,22 +11,28 @@ app.set("views" /*what to change*/, "./views/ejs"/*new directory name*/); // if 
 app.listen(3000); // automatically infers to the localhost
 
 app.get("/", (req, res) => {
-    res.render("index"); // will look for `index.ejs` file inside `views` directory
+    const blogs = [
+        {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur"},
+        {title: "Mario finds starts", snippet: "Lorem ipsum dolor sit amet consectetur"},
+        {title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur"},
+    ]
+    res.render("index", {title: 'Home', blogs: blogs}); // will look for `index.ejs` file inside `views` directory
+    // create `title` var with value `Home` and can use it as a js inside `index.ejs` file
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", {title: 'About'});
 });
 
 //redirect will work automatically
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create");
+    res.render("create", {title: 'Create a new Blog'});
 })
 
 // 404 page
 // it works when no response was successfull.
 // must be in the bottom of all routes. routes under it won't work
 app.use((req, res) => { 
-    res.status(404).render("404");
+    res.status(404).render("404", {title: '404'});
 })
