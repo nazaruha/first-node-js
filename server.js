c21650203f12f2fc1b7291aa7ff9be69ac6b8c37
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-22 13:04:24 +0200

    send simple html response to the browser

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     //set header content type
-    res.setHeader("Content-Type", "text/plain" /*sending some text to the browser*/);
-    res.write("hello, ninjas"); // what content send to the browser
+    res.setHeader("Content-Type", "text/html" /*sending some text to the browser*/);
+    res.write("<h1 style=\"color: red;\">hello, ninjas</h1>"); // what content send to the browser
     res.end(); // end the response
 }); // method to create a server
 
