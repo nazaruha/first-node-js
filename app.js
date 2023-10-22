5fc1e3983ea0ea29e63f5ea9600c9bda3846b2f3
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-22 18:25:35 +0200

    make view using EJS

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ const express = require("express");
 // express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs'); // the default directory for EJSs is the 'views' directory
+app.set("views" /*what to change*/, "./views/ejs"/*new directory name*/); // if you have another directory for views - set up another direcoty name for it for exmpl: 'myviews'
+
 // listen for requests
 app.listen(3000); // automatically infers to the localhost
 
 app.get("/", (req, res) => {
-    // this `send` method automatically recognises what kind of data you want to send
-    //res.send("<p>home page</p>"); 
-
-    // `sendFile` method - how to easy export html files.
-    // BUT you need to pass the root directory, 'cause express doesn't where from to start
-    res.sendFile("./views/index.html", {root: __dirname});
+    res.render("index"); // will look for `index.ejs` file inside `views` directory
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile("./views/about.html", {root: __dirname});
+    res.render("about");
 });
 
-//redirects
-app.get("/about-us", (req, res) => {
-    res.redirect("/about"); // that's it... easy
-});
+//redirect will work automatically
+
+app.get("/blogs/create", (req, res) => {
+    res.render("create");
+})
 
 // 404 page
 // it works when no response was successfull.
 // must be in the bottom of all routes. routes under it won't work
 app.use((req, res) => { 
-    res.status(404).sendFile("./views/404.html", {root: __dirname});
+    res.status(404).render("404");
 })
